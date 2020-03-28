70f15154550a391dd599d8321165d7f0c3d70011
Md. Shaiful Islam <islambuet@gmail.com>
2020-03-28 07:03:48 +0600

    bi_2019_20 upload included

--- application/config/system.php
+++ application/config/system.php
@@ -1,6 +1,6 @@
 <?php
 $config['system_site_root_folder']='api_file_server';
-$config['system_allowed_sites']=array('login_2018_19','beeztola_2018_19','ems_2018_19','sms_2018_19','fms_2018_19','bms_2018_19');
+$config['system_allowed_sites']=array('login_2018_19','beeztola_2018_19','ems_2018_19','sms_2018_19','fms_2018_19','bms_2018_19','bi_2019_20');
 $config['system_allowed_auth_keys']=array('ems_2018_19');
 
 
